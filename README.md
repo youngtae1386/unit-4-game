115dc3b83d1c10ff9c9f81fbc4dcaa58d9228afb
Young Cho <youngtae1386@gmail.com>
2018-12-21 19:12:15 -0600

    bug fixes and readme.md

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # unit-4-game
 Crystal Game
+
+## File
+
+* [`Crystal Game`](https://youngtae1386.github.io/unit-4-game/)
+
+## Instructions
+* Page load with 4 different type of crystals.  **red**,**blue**,**green**,**yello**.
+* By clicking on a crystal, you will *add* a specific amount of points to your *total score*.
+* You win the game by matching your total score to the Random Generator. 
+* You lose the game if your total score goes above the Random Generator 
+* The value of each crystal is hidden from you until you click on it.
+* Each time when the game starts, the game will change the values of each crystal.
+
+ > Rules
+
+- *Alert - You Win: You Matched Random #*
+- *Alert - You Lost: You Went Over the random #*
+
+> **Have Fun**  
--- assets/main.js
+++ assets/main.js
@@ -5,21 +5,21 @@ var wins = 0;
 var counter = 0;
 var numberOfClickEveryEachGame = 0;
 
-  var images = [
-    "images/yellow.png",
-    "images/red.png",
-    "images/blue.png",
-    "images/green.png"
-  ];
-var startCrystalgame = function() {
-    $(".crystals").empty();
-   
+var images = [
+  "images/yellow.png",
+  "images/red.png",
+  "images/blue.png",
+  "images/green.png"
+];
+var startCrystalgame = function () {
+  $(".crystals").empty();
+
   random_Numb = Math.floor(Math.random() * 120 + 19);
-$("#result").html("Random Generator: " + random_Numb); // random number for "result"
- for (var i = 0; i < 4; i++) {
-    
+  $("#random_Numb").html("Random Generator: " + random_Numb); // random number for "result"
+  for (var i = 0; i < 4; i++) {
+
     var random = Math.floor(Math.random() * 11 + 1);
-    
+
     var crystal = $("<img>");
     crystal.attr({
       "class": 'crystal',
@@ -35,8 +35,8 @@ $("#result").html("Random Generator: " + random_Numb); // random number for "res
   }
 
   //(crystal) on click function will start ... 
-  $(".crystal").on("click", function() {
-    var crystalValue = parseInt ($(this).attr("data-crystalvalue"));
+  $(".crystal").on("click", function () {
+    var crystalValue = parseInt($(this).attr("data-crystalvalue"));
     counter += crystalValue; // counter + Crystal value
     console.log(crystalValue);
 
@@ -56,7 +56,7 @@ $("#result").html("Random Generator: " + random_Numb); // random number for "res
       $("#counter").html("Total Scores: 0 ");
       alert("You Lost: You Went Over the random #: " + random_Numb);
       startCrystalgame();//call back - starts the game when lost.
-    } 
+    }
     else if (counter === random_Numb) {
       wins++;
       $("#wins").html("Wins: " + wins);
